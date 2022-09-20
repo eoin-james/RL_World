dfc18a7cf291b3d4e75ac8466c3f7f4248476a21
eoin-james <eoin.james@mcallister.ai>
2022-09-20 13:01:14 +0100

    Started dev on Q Learning IPYNB

---
+++ example.py
@@ -0,0 +1,21 @@
+import gym
+
+# import MODEL
+MODEL = None
+
+env_name = ""
+env = gym.make(env_name)
+
+model = MODEL(env)
+
+model.learn(total_timesteps=10_000)
+
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+      obs = env.reset()
+
+env.close()
